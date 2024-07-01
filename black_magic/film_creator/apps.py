4930940bfa87c56bd4005986a76443cf5daf2aa1
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-07-01 18:54:21 +0100

    general progress

--- black_magic/film_creator/apps.py
+++ black_magic/film_creator/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class FilmCreatorConfigdjango(AppConfig):
+class FilmCreatorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "film_creator"
+
+
--- black_magic/film_creator/management/commands/populate_data.py
+++ black_magic/film_creator/management/commands/populate_data.py
@@ -1,13 +1,7 @@
 # film_creator/management/commands/populate_data.py
-import sys
-import os
-
 from django.core.management.base import BaseCommand
-from black_magic.film_creator.models import Actor, Film, Role
 from django.db import connection
-
-# Adding the root BlackFilmMagician/black_magic to the path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
+from black_magic.film_creator.models import Actor, Film, Role
 
 
 class Command(BaseCommand):
@@ -20,10 +14,9 @@ class Command(BaseCommand):
                            'as "actor_picture" FROM public.actor_model;')
             actors_data = cursor.fetchall()
 
-            cursor.execute(
-                'SELECT "Film_IMDb_ID" as "film_imdb_id", "Title" as "title", "Release_Date" as "release_date", '
-                '"Genre" as "genre", "Plot" as "plot", "Plot_Words" as "plot_words", "Keyword_List" as '
-                '"keyword_list", "Worldwide_Gross" as "worldwide_gross" FROM public.film_model;')
+            cursor.execute('SELECT "Film_IMDb_ID" as "film_imdb_id", "Title" as "title", "Release_Date" as "release_date", '
+                           '"Genre" as "genre", "Plot" as "plot", "Plot_Words" as "plot_words", "Keyword_List" as '
+                           '"keyword_list", "Worldwide_Gross" as "worldwide_gross" FROM public.film_model;')
             films_data = cursor.fetchall()
 
             cursor.execute('SELECT "Actor_TMDb_ID" as "actor_tmdb_id",  "Film_IMDb_ID" as "film_imdb_id", "Title" as '
@@ -46,10 +39,10 @@ class Command(BaseCommand):
         for imdb_id, title, release_date, genre, plot, plot_words, keyword_list, worldwide_gross in films_data:
             Film.objects.get_or_create(
                 film_imdb_id=imdb_id,
-                defaults={'imdb_id': imdb_id, 'title': title, 'release_date': release_date, 'genre': genre,
-                          'plot': plot,  #
+                defaults={'imdb_id': imdb_id, 'title': title, 'release_date': release_date, 'genre': genre, 'plot': plot,
                           'plot_words': plot_words, 'keyword_list': keyword_list, 'worldwide_gross': worldwide_gross}
             )
+
         # Create Role instances
         for actor_tmdb_id, film_imdb_id, title, actor_name, lead_actor in roles_data:
             actor = Actor.objects.get(actor_tmdb_id=actor_tmdb_id)
