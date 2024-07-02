25e83704e80b3c785fe305628f27f08b680725ca
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-07-02 12:48:55 +0100

    general progress

--- film_creator/management/commands/populate_data.py
+++ film_creator/management/commands/populate_data.py
@@ -1,7 +1,8 @@
 # film_creator/management/commands/populate_data.py
 from django.core.management.base import BaseCommand
 from django.db import connection
-from black_magic.film_creator.models import Actor, Film, Role
+from film_creator.models import Actor, Film, Role
+from psycopg2 import errors
 
 
 class Command(BaseCommand):
@@ -14,7 +15,7 @@ class Command(BaseCommand):
                            'as "actor_picture" FROM public.actor_model;')
             actors_data = cursor.fetchall()
 
-            cursor.execute('SELECT "Film_IMDb_ID" as "film_imdb_id", "Title" as "title", "Release_Date" as "release_date", '
+            cursor.execute('SELECT "IMDb_ID" as "film_imdb_id", "Title" as "title", "Release_Date" as "release_date", '
                            '"Genre" as "genre", "Plot" as "plot", "Plot_Words" as "plot_words", "Keyword_List" as '
                            '"keyword_list", "Worldwide_Gross" as "worldwide_gross" FROM public.film_model;')
             films_data = cursor.fetchall()
@@ -26,8 +27,8 @@ class Command(BaseCommand):
         # Create Actor instances
         for actor_tmdb_id, actor_imdb_id, name, gender, birthday, movie_credits, picture in actors_data:
             Actor.objects.get_or_create(
-                actor_tmdb_id=actor_tmdb_id,
-                defaults={'actor_imdb_id': actor_imdb_id,
+                tmdb_id=actor_tmdb_id,
+                defaults={'imdb_id': actor_imdb_id,
                           'name': name,
                           'gender': gender,
                           'birthday': birthday,
@@ -38,19 +39,28 @@ class Command(BaseCommand):
         # Create Film instances
         for imdb_id, title, release_date, genre, plot, plot_words, keyword_list, worldwide_gross in films_data:
             Film.objects.get_or_create(
-                film_imdb_id=imdb_id,
+                imdb_id=imdb_id,
                 defaults={'imdb_id': imdb_id, 'title': title, 'release_date': release_date, 'genre': genre, 'plot': plot,
                           'plot_words': plot_words, 'keyword_list': keyword_list, 'worldwide_gross': worldwide_gross}
             )
 
         # Create Role instances
         for actor_tmdb_id, film_imdb_id, title, actor_name, lead_actor in roles_data:
-            actor = Actor.objects.get(actor_tmdb_id=actor_tmdb_id)
-            film = Film.objects.get(film_imdb_id=film_imdb_id)
+            try:
+                actor = Actor.objects.get(tmdb_id=actor_tmdb_id)
+            except Actor.DoesNotExist:
+                self.stdout.write(self.style.WARNING(f'Actor with tmdb_id {actor_tmdb_id} does not exist. Skipping...'))
+                continue
+
+            try:
+                film = Film.objects.get(imdb_id=film_imdb_id)
+            except Film.DoesNotExist:
+                self.stdout.write(self.style.WARNING(f'Film with imdb_id {film_imdb_id} does not exist. Skipping...'))
+                continue
+
             Role.objects.get_or_create(
-                actor_tmdb_id=actor_tmdb_id,
-                defaults={'actor_tmdb_id': actor_tmdb_id, 'film_imdb_id': film_imdb_id, 'title': title,
-                          'actor_name': actor_name, 'lead_actor': lead_actor}
+                defaults={'actor': actor, 'film': film, 'film_title': title,
+                          'actor_name': actor_name, 'lead': lead_actor}
             )
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with initial data'))
--- film_creator/models.py
+++ film_creator/models.py
@@ -3,32 +3,34 @@ from django.db import models
 
 # Create your models here.
 class Actor(models.Model):
+    DoesNotExist = None
     objects = models.Manager()
-    tmdb_id = models.IntegerField(max_length=15, primary_key=True)
+    tmdb_id = models.IntegerField(primary_key=True)
     imdb_id = models.CharField(max_length=15)
     name = models.CharField(max_length=100)
-    gender = models.IntegerField(max_length=1)
-    birthday = models.DateTimeField()
+    gender = models.IntegerField()
+    birthday = models.DateField(null=True, blank=True)
     movie_credits = models.CharField()
     picture = models.URLField()
 
 
 class Film(models.Model):
+    DoesNotExist = None
     objects = models.Manager()
     imdb_id = models.CharField(max_length=15, primary_key=True)
     title = models.CharField(max_length=150)
-    release_date = models.DateTimeField()
+    release_date = models.DateField()
     genre = models.CharField()
     plot = models.TextField()
     plot_words = models.TextField()
     keyword_list = models.TextField()
-    worldwide_gross = models.IntegerField()
+    worldwide_gross = models.BigIntegerField(null=True, blank=True)
 
 
 class Role(models.Model):
     objects = models.Manager()
-    film_imdb_id = models.ForeignKey(Film, on_delete=models.CASCADE)
-    actor_tmdb_id = models.CharField(max_length=15)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name='actor_roles', null=True, blank=True)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name='roles', null=True, blank=True)
     film_title = models.CharField(max_length=150)
     actor_name = models.CharField(max_length=50)
-    lead = models.IntegerField(max_length=1)
+    lead = models.IntegerField()
