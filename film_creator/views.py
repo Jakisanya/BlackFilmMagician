114a381270e3f40770cff529bdfc3b6eedb6850c
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-07-08 11:56:16 +0100

    general progress

--- film_creator/views.py
+++ film_creator/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import TemplateView
-
+from .models import Actor
+import random
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -21,6 +22,12 @@ class CreateView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Create Page'
         context['message'] = 'Get creating!'
+
+        # Fetch all actors and select three at random
+        actors = list(Actor.objects.all())
+        selected_actors = random.sample(actors, 3) if len(actors) >= 3 else actors
+        context['actors'] = selected_actors
+
         return context
 
     def create(request):
