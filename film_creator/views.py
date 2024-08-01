ebf51d124a2d186576e01bdfe0e747bc24eeb6c8
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-08-01 11:00:42 +0100

    general progress

--- film_creator/views.py
+++ film_creator/views.py
@@ -23,6 +23,7 @@ class HomeView(TemplateView):
 class CreateView(TemplateView):
     template_name = 'create.html'
 
+    """
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Create Page'
@@ -34,7 +35,7 @@ class CreateView(TemplateView):
         selected_actors = random.sample(actors, 3) if len(actors) >= 3 else actors
         context['actors'] = selected_actors
         return context
-
+    """
 
 def fetch_new_actors():
     # Get all unique actor IDs from roles where lead is 1
@@ -69,9 +70,6 @@ def generate_gpt_film_details(request):
     # print(f'actor_film_ids: {actor_film_ids}')
     lead_actor_films = Film.objects.filter(imdb_id__in=actor_film_ids)
 
-    # print(f'lead_actor_roles: {len(list(lead_actor_roles))}')
-    # print(f'lead_actor_films: {len(list(lead_actor_films))}')
-
     # Convert the querysets to lists of dictionaries
     lead_actor_info_list = [{'name': actor.name, 'picture': actor.picture, 'tmdb_id': actor.tmdb_id} for actor in
                             lead_actor_info]
@@ -83,60 +81,62 @@ def generate_gpt_film_details(request):
     lead_actor_roles_json = json.dumps(lead_actor_roles_list, indent=4)
     lead_actor_films_json = json.dumps(lead_actor_films_list, indent=4)
 
-    # Query to find roles for the specific actor and actor_names
-    prompt = (f"Create a film title and plot for a new movie starring the following lead actors {', '.join(actor_names)}. "
-              f"Use the following data as reference:"
-              f"1. Lead Actor Info: {lead_actor_info_json}\n"
-              f"2. Lead Actor Roles: {lead_actor_roles_json}\n"
-              f"3. Lead Actor Films: {lead_actor_films_json}\n\n" 
-              
-              f"Here's how I want you to use the reference data:"
-              f"Cross reference the data presented in 'Lead Actor Info', 'Lead Actor Roles', and 'Lead Actor Films' to "
-              f"help categorise and gain an understanding of each actor and to help create a synergistic and cohesive "
-              f"new film idea.\n"
-              f"1. Use 'Lead Actor Info' to get information about each actor. Within 'Lead Actor Info', factor in "
-              f"their age from 'birthday' and their 'gender', when creating their character and role in the new film.\n"
-              f"2. Use 'Lead Actor Roles' to understand the roles each actor has played. Within 'fields' in "
-              f"'Lead Actor Roles', find the lead actor in 'actor', the 'film', and if they were 'lead' to decide "
-              f"whether to weigh the data for that film more heavily if they were lead than if they were not when "
-              f"building their character and role in the new film.\n"
-              f"3. Use 'Lead Actor Films' to see the films each actor has been in. Within 'fields' in "
-              f"'Lead Actor Films', use 'genre', 'plot', 'plot_words', 'keyword_list' to gain an understanding of the "
-              f"genre of film they are most suited to. Use the information to find a single genre that is most suited "
-              f"to all of the lead actors in the new film. The new plot should clearly outline a film that sits in "
-              f"this single genre and is suited to all of the actors to the highest degree. Also use the information "
-              f"to help create the new film title and plot. The film title should creatively convey what the film is "
-              f"about. The plot should be clear, descriptive, and less than 500 words. It should specifically mention "
-              f"each lead actor and their character and role in the film. Use the style of writing of the plots of "
-              f"highest grossing films to gain an understanding of how to write the new plot. The gross figure is "
-              f"referenced in 'worldwide_gross'. \n"
-              f"4. In addition to the film title and plot, provide 5 detailed bullet points explaining how the film "
-              f"has been created - making reference to the reference data used. The last bullet point should always "
-              f"explain how the title was created, i.e., the inspiration behind the title.\n\n"
-              f"The model's response should have 3 sections: 'Title', 'Plot' and 'Reasoning'.")
-
-    # print(f"Prompt: {prompt}\n\n")
-
-    client = OpenAI()
-
-    assistant = client.beta.assistants.create(
-        name="Film Magician",
-        instructions="",
-        tools=[{"type": ""}],
-        model="gpt-4o-mini",
-    )
-
-    my_thread_message = client.beta.threads.messages.create(
-        thread_id=my_thread.id,
-        role="user",
-        content=prompt,
-        attachments=[
-            {"file_id": file_id, "tools": [{"type": "file_search"}]}
-        ],
-    )
+    return JsonResponse({"lead_actor_info": lead_actor_info_json})  # "gpt_film_details": film_details
 
 
+"""
+# Query to find roles for the specific actor and actor_names
+prompt = (
+    f"Create a film title and plot for a new movie starring the following lead actors {', '.join(actor_names)}. "
+    f"Use the following data as reference:"
+    f"1. Lead Actor Info: {lead_actor_info_json}\n"
+    f"2. Lead Actor Roles: {lead_actor_roles_json}\n"
+    f"3. Lead Actor Films: {lead_actor_films_json}\n\n"
 
-    print(f'Film details: {film_details}')
+    f"Here's how I want you to use the reference data:"
+    f"Cross reference the data presented in 'Lead Actor Info', 'Lead Actor Roles', and 'Lead Actor Films' to "
+    f"help categorise and gain an understanding of each actor and to help create a synergistic and cohesive "
+    f"new film idea.\n"
+    f"1. Use 'Lead Actor Info' to get information about each actor. Within 'Lead Actor Info', factor in "
+    f"their age from 'birthday' and their 'gender', when creating their character and role in the new film.\n"
+    f"2. Use 'Lead Actor Roles' to understand the roles each actor has played. Within 'fields' in "
+    f"'Lead Actor Roles', find the lead actor in 'actor', the 'film', and if they were 'lead' to decide "
+    f"whether to weigh the data for that film more heavily if they were lead than if they were not when "
+    f"building their character and role in the new film.\n"
+    f"3. Use 'Lead Actor Films' to see the films each actor has been in. Within 'fields' in "
+    f"'Lead Actor Films', use 'genre', 'plot', 'plot_words', 'keyword_list' to gain an understanding of the "
+    f"genre of film they are most suited to. Use the information to find a single genre that is most suited "
+    f"to all of the lead actors in the new film. The new plot should clearly outline a film that sits in "
+    f"this single genre and is suited to all of the actors to the highest degree. Also use the information "
+    f"to help create the new film title and plot. The film title should creatively convey what the film is "
+    f"about. The plot should be clear, descriptive, and less than 500 words. It should specifically mention "
+    f"each lead actor and their character and role in the film. Use the style of writing of the plots of "
+    f"highest grossing films to gain an understanding of how to write the new plot. The gross figure is "
+    f"referenced in 'worldwide_gross'. \n"
+    f"4. In addition to the film title and plot, provide 5 detailed bullet points explaining how the film "
+    f"has been created - making reference to the reference data used. The last bullet point should always "
+    f"explain how the title was created, i.e., the inspiration behind the title.\n\n"
+    f"The model's response should have 3 sections: 'Title', 'Plot' and 'Reasoning'.")
 
-    return JsonResponse({"lead_actor_info": lead_actor_info, "gpt_film_details": film_details})
+# print(f"Prompt: {prompt}\n\n")
+
+client = OpenAI()
+
+assistant = client.beta.assistants.create(
+    name="Film Magician",
+    instructions="",
+    tools=[{"type": ""}],
+    model="gpt-4o-mini",
+)
+
+my_thread_message = client.beta.threads.messages.create(
+    thread_id=my_thread.id,
+    role="user",
+    content=prompt,
+    attachments=[
+        {"file_id": file_id, "tools": [{"type": "file_search"}]}
+    ],
+)
+
+print(f'Film details: {film_details}')
+"""
