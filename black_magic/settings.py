a77540d2b60d3f89617f7beddcb7eee0752d07fd
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-08-11 18:44:14 +0100

    general progress

--- black_magic/settings.py
+++ black_magic/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
-# For production, define STATIC_ROOT
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
