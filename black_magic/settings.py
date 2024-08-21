77d097c98bc7ed61b796c050479fa78f8e2887c4
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-08-21 18:41:20 +0100

    general progress

--- black_magic/settings.py
+++ black_magic/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    "film_creator/static/film_creator/images/"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
